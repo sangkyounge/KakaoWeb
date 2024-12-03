fe8f118aff7bbf57b5a84ae5b3a7204d497cb2ea
Sangkyounge <shin8877123@naver.com>
2024-12-03 11:32:12 +0900

    12주차 실습 과제

--- space-game/app.js
+++ space-game/app.js
@@ -38,6 +38,7 @@ const Messages = {
     KEY_EVENT_SPACE: "KEY_EVENT_SPACE",
     COLLISION_ENEMY_LASER: "COLLISION_ENEMY_LASER",
     COLLISION_ENEMY_HERO: "COLLISION_ENEMY_HERO",
+    COLLISION_COMPANION_LASER: "COLLISION_COMPANION_LASER",
 };
 
 // 4. 기본 게임 객체 클래스
@@ -74,6 +75,31 @@ class Hero extends GameObject {
         this.height = 75;
         this.type = 'Hero';
         this.cooldown = 0;
+        
+        // 메인 우주선과의 간격과 크기 비율 설정
+        const WING_SPACING = 40;
+        const WING_SCALE = 0.5;
+        const companionWidth = this.width * WING_SCALE;
+        
+        this.companions = [
+            new Companion(x - WING_SPACING - companionWidth, y + 10, true),
+            new Companion(x + this.width + WING_SPACING, y + 10, false)
+        ];
+        gameObjects.push(...this.companions);
+    }
+
+    moveX(distance) {
+        this.x += distance;
+        this.companions.forEach(companion => {
+            companion.x += distance;
+        });
+    }
+
+    moveY(distance) {
+        this.y += distance;
+        this.companions.forEach(companion => {
+            companion.y += distance;
+        });
     }
 
     fire() {
@@ -93,9 +119,49 @@ class Hero extends GameObject {
     canFire() {
         return this.cooldown === 0;
     }
+
+    updateCompanions() {
+        this.companions.forEach(companion => {
+            if (companion.canFire()) {
+                companion.fire();
+            }
+        });
+    }
 }
 
-// 6. 적 클래스
+// 6. 컴패니언 클래스
+class Companion extends GameObject {
+    constructor(x, y, isLeft) {
+        super(x, y);
+        this.width = 99 * 0.5;  // 메인 우주선 크기의 0.5배
+        this.height = 75 * 0.5;
+        this.type = 'Companion';
+        this.isLeft = isLeft;
+        this.cooldown = 0;
+        this.img = heroImg;
+    }
+
+    fire() {
+        if (this.canFire()) {
+            const laserX = this.x + this.width / 2 - 2;
+            gameObjects.push(new CompanionLaser(laserX, this.y - 10));
+            this.cooldown = 1000;
+            let id = setInterval(() => {
+                if (this.cooldown > 0) {
+                    this.cooldown -= 100;
+                } else {
+                    clearInterval(id);
+                }
+            }, 100);
+        }
+    }
+
+    canFire() {
+        return this.cooldown === 0;
+    }
+}
+
+// 7. 적 클래스
 class Enemy extends GameObject {
     constructor(x, y) {
         super(x, y);
@@ -113,7 +179,7 @@ class Enemy extends GameObject {
     }
 }
 
-// 7. 레이저 클래스
+// 8. 레이저 클래스
 class Laser extends GameObject {
     constructor(x, y) {
         super(x, y);
@@ -132,7 +198,47 @@ class Laser extends GameObject {
     }
 }
 
-// 8. 충돌 감지 함수
+// 9. 컴패니언 레이저 클래스
+class CompanionLaser extends GameObject {
+    constructor(x, y) {
+        super(x, y);
+        this.width = 5;
+        this.height = 15;
+        this.type = 'CompanionLaser';
+        this.img = laserImg;
+        let id = setInterval(() => {
+            if (this.y > 0) {
+                this.y -= 15;
+            } else {
+                this.dead = true;
+                clearInterval(id);
+            }
+        }, 100);
+    }
+}
+
+// 10. 폭발 클래스
+class Explosion extends GameObject {
+    constructor(x, y) {
+        super(x, y);
+        this.width = 50;
+        this.height = 50;
+        this.type = 'Explosion';
+        this.frame = 0;
+        this.totalFrames = 10;
+        this.img = explosionImg;
+        
+        let id = setInterval(() => {
+            this.frame++;
+            if (this.frame >= this.totalFrames) {
+                this.dead = true;
+                clearInterval(id);
+            }
+        }, 50);
+    }
+}
+
+// 11. 충돌 감지 함수
 function intersectRect(r1, r2) {
     return !(
         r2.left > r1.right ||
@@ -142,7 +248,7 @@ function intersectRect(r1, r2) {
     );
 }
 
-// 9. 게임 객체 생성 함수들
+// 12. 게임 객체 생성 함수들
 function createEnemies() {
     const MONSTER_TOTAL = 5;
     const MONSTER_WIDTH = MONSTER_TOTAL * 98;
@@ -166,45 +272,52 @@ function createHero() {
     gameObjects.push(hero);
 }
 
-// 10. 게임 객체 그리기 함수
+// 13. 게임 객체 그리기 함수
 function drawGameObjects(ctx) {
     gameObjects.forEach(go => go.draw(ctx));
 }
 
-// 11. 게임 객체 업데이트 함수
+// 14. 게임 객체 업데이트 함수
 function updateGameObjects() {
-    const enemies = gameObjects.filter((go) => go.type === "Enemy");
-    const lasers = gameObjects.filter((go) => go.type === "Laser");
-    lasers.forEach((l) => {
-        enemies.forEach((m) => {
-            if (intersectRect(l.rectFromGameObject(), m.rectFromGameObject())) {
-                eventEmitter.emit(Messages.COLLISION_ENEMY_LASER, {
-                    first: l,
-                    second: m,
-                });
+    const enemies = gameObjects.filter(go => go.type === "Enemy");
+    const lasers = gameObjects.filter(go => go.type === "Laser" || go.type === "CompanionLaser");
+    
+    if (hero) {
+        hero.updateCompanions();
+    }
+
+    lasers.forEach(laser => {
+        enemies.forEach(enemy => {
+            if (intersectRect(laser.rectFromGameObject(), enemy.rectFromGameObject())) {
+                gameObjects.push(new Explosion(enemy.x, enemy.y));
+                eventEmitter.emit(
+                    laser.type === "Laser" ? Messages.COLLISION_ENEMY_LASER : Messages.COLLISION_COMPANION_LASER,
+                    { first: laser, second: enemy }
+                );
             }
         });
     });
-    gameObjects = gameObjects.filter((go) => !go.dead);
+
+    gameObjects = gameObjects.filter(go => !go.dead);
 }
 
-// 12. 게임 초기화 함수
+// 15. 게임 초기화 함수
 function initGame() {
     gameObjects = [];
     createEnemies();
     createHero();
 
     eventEmitter.on(Messages.KEY_EVENT_UP, () => {
-        hero.y -= 5;
+        hero.moveY(-5);
     });
     eventEmitter.on(Messages.KEY_EVENT_DOWN, () => {
-        hero.y += 5;
+        hero.moveY(5);
     });
     eventEmitter.on(Messages.KEY_EVENT_LEFT, () => {
-        hero.x -= 5;
+        hero.moveX(-5);
     });
     eventEmitter.on(Messages.KEY_EVENT_RIGHT, () => {
-        hero.x += 5;
+        hero.moveX(5);
     });
     eventEmitter.on(Messages.KEY_EVENT_SPACE, () => {
         if (hero.canFire()) {
@@ -215,16 +328,19 @@ function initGame() {
         first.dead = true;
         second.dead = true;
     });
+    eventEmitter.on(Messages.COLLISION_COMPANION_LASER, (_, { first, second }) => {
+        first.dead = true;
+        second.dead = true;
+    });
 }
 
-// 13. 전역 변수 선언
-let canvas, ctx,
-    heroImg, enemyImg, laserImg,
-    gameObjects = [],
-    hero,
-    eventEmitter = new EventEmitter();
+// 16. 전역 변수 선언
+let canvas, ctx, heroImg, enemyImg, laserImg, explosionImg;
+let gameObjects = [];
+let hero;
+let eventEmitter = new EventEmitter();
 
-// 14. 키보드 이벤트 리스너
+// 17. 키보드 이벤트 리스너
 window.addEventListener("keyup", (evt) => {
     if (evt.key === "ArrowUp") {
         eventEmitter.emit(Messages.KEY_EVENT_UP);
@@ -239,13 +355,14 @@ window.addEventListener("keyup", (evt) => {
     }
 });
 
-// 15. 게임 시작
+// 18. 게임 시작
 window.onload = async () => {
     canvas = document.getElementById("myCanvas");
     ctx = canvas.getContext("2d");
     heroImg = await loadTexture("asset/player.png");
     enemyImg = await loadTexture("asset/enemyShip.png");
     laserImg = await loadTexture("asset/laserRed.png");
+    explosionImg = await loadTexture("asset/laserGreenShot.png");
     
     initGame();
     
